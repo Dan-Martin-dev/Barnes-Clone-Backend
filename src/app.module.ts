56884fa0a9bc6478d76ddf1ada7b8ecedd203a2b
Vare <dm.va@hotmail.com>
2024-08-30 10:27:12 -0300

    Setting up migrations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { dataSourceOptions } from 'db/data-source.';
 import { UsersModule } from './users/users.module';
+import { dataSourceOptions } from 'db/data-source';
 
 @Module({
   imports: [TypeOrmModule.forRoot(dataSourceOptions), UsersModule],
