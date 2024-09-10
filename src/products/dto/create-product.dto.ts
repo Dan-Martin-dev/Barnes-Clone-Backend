d64f22e5cf9e0838afb6c2b7573dc902219ad512
Vare <dm.va@hotmail.com>
2024-09-10 09:39:10 -0300

    CreatedProductDto created

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,39 @@
-export class CreateProductDto {}
+import {
+  IsArray,
+  IsNotEmpty,
+  IsNumber,
+  IsPositive,
+  IsString,
+  Min,
+} from 'class-validator';
+
+export class CreateProductDto {
+  @IsNotEmpty({ message: 'title can not be empty' })
+  @IsString()
+  title: string;
+
+  @IsNotEmpty({ message: 'description can not be empty' })
+  @IsString()
+  description: string;
+
+  @IsNotEmpty({ message: 'price can not be empty' })
+  @IsNumber(
+    { maxDecimalPlaces: 2 },
+    { message: 'price should be a number & max decimal precission 2' },
+  )
+  @IsPositive({ message: 'price should be a positive number' })
+  price: number;
+
+  @IsNotEmpty({ message: 'stock can not be empty' })
+  @IsNumber({}, { message: 'stock should be a number' })
+  @Min(0, { message: 'stock can not be negative.' })
+  stock: number;
+
+  @IsNotEmpty({ message: 'images can not be empty' })
+  @IsArray({ message: 'images should be in array format' })
+  images: string[];
+
+  @IsNotEmpty({ message: 'categories can not be empty' })
+  @IsNumber({}, { message: 'category id should be a number' })
+  category: number;
+}
